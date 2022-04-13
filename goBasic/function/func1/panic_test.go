19ec4f293561c386c223b9f1eea96654630f62cc
yyangbobo <1360664142@qq.com>
2022-04-13 15:27:45 +0800

    golang第四天

--- goBasic/function/func1/panic_test.go
+++ goBasic/function/func1/panic_test.go
@@ -1,6 +1,9 @@
 package func1
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestPanic(t *testing.T){
 	panicTest()
@@ -9,10 +12,24 @@ func TestPanic(t *testing.T){
 
 func panicTest() {
 	defer func() {
+		//捕获错误
 		if err := recover(); err != nil {
 			println(err.(string)) // 将 interface{} 转型为具体类型。
 		}
 	}()
-
+	//抛出错误
 	panic("panic error!")
 }
+
+func TestPanic1(t *testing.T){
+	defer func() {
+		if err := recover();err!=nil{
+			fmt.Println(err)
+		}
+	}()
+
+	var ch chan int = make(chan int,10)
+	close(ch)
+	ch<-1
+}
+
